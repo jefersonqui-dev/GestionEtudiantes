388a268faf594566ef888108c171cc2f07f0bb16
Jeferson Quiguantar <jeffersonquiguantar@gmail.com>
2025-06-17 08:48:47 -0500

    Busqueda por nombre usando RequestParam

--- tutoria/src/main/java/com/devsenior/jquiguantar/tutoria/controller/UserController.java
+++ tutoria/src/main/java/com/devsenior/jquiguantar/tutoria/controller/UserController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsenior.jquiguantar.tutoria.model.dto.UserDto;
 @RestController
+@RequestMapping("/users")
 public class UserController {
     private List<UserDto> users = new ArrayList<>(List.of(
         new UserDto("Juan Pérez", "juan.perez@email.com", "pass123", true),
@@ -27,24 +29,31 @@ public class UserController {
         new UserDto("Diego Fernández", "diego.fernandez@email.com", "pass369", true),
         new UserDto("Elena Ruiz", "elena.ruiz@email.com", "pass741", true)
     ));
-    @GetMapping("/users")
+    @GetMapping
     public List<UserDto> getAll(){
         return users ;
     }
-    @GetMapping("/users/{pos}")
+    @GetMapping("/{pos}")
     public UserDto getById(@PathVariable int pos){
         return users.get(pos);
     }
-    @PostMapping("/users")
+    @PostMapping
     public UserDto create(@RequestBody UserDto user){
         users.add(user);
         return user;
     }
-    @DeleteMapping("/users/{pos}")
+    @DeleteMapping("/{pos}")
     public UserDto delete(@PathVariable int pos){
         return users.remove(pos);
     }
-    @PutMapping("/users/{pos}")
+    @GetMapping("/name")
+    public UserDto getByName(@RequestParam String name){
+        return users.stream()
+        .filter(user -> user.getName().equals(name))
+        .findFirst()
+        .orElse(null);
+    }
+    @PutMapping("/{pos}")
     public UserDto update(@PathVariable int pos, @RequestBody UserDto user){
         users.set(pos, user);
         return user;
